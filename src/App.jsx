fc7fa794fbadc192f4437cd06bf7de759216fb1d
Mohit Chauhan <mohitchauhanwrk@gmail.com>
2026-09-08 21:58:33 +0530

    added font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div className="bg-neutral-800 text-amber-50">
-      <div className="conntainer max-w-300 bg-neutral-800 text-amber-50 mx-auto grid grid-rows-2">
+    <div className="bg-neutral-800 h-screen text-amber-50">
+      <div className="conntainer font-supreme font-medium  max-w-300 bg-neutral-800 text-amber-50 mx-auto grid grid-rows-2">
         <nav className="navbar h-20">NavBar</nav>
         <main className="main_content h-auto">Main Content</main>
       </div>
