0acd75b9acbc3b3c5cdedeed16a993eb76ef65da
Mohit Chauhan <mohitchauhanwrk@gmail.com>
2026-09-08 22:22:43 +0530

    built the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <div className="bg-neutral-800 h-screen text-amber-50">
       <div className="conntainer font-supreme font-medium  max-w-300 bg-neutral-800 text-amber-50 mx-auto grid grid-rows-2">
-        <nav className="navbar h-20">NavBar</nav>
+        <Navbar />
         <main className="main_content h-auto">Main Content</main>
       </div>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar h-20 flex justify-between border-b-2 items-center">
+      <h1 className="font-extrabold text-2xl cursor-pointer">MovieMouse</h1>
+      <div className="navbar_links flex gap-3 text-2xl">
+        <a
+          className="hover:bg-neutral-600 transition-all duration-300 h-fit px-3 py-1 rounded-md"
+          href=""
+        >
+          Popular 🔥
+        </a>
+        <a
+          className="hover:bg-neutral-600 transition-all duration-300 h-fit px-3 py-1 rounded-md"
+          href=""
+        >
+          Top Rated ⭐️
+        </a>
+        <a
+          className="hover:bg-neutral-600 transition-all duration-300 h-fit px-3 py-1 rounded-md"
+          href=""
+        >
+          Upcoming 🥳
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
