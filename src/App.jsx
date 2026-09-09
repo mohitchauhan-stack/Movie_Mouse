08061ca6baafca9854838e119b5a1e5b49760ca9
Mohit Chauhan <mohitchauhanwrk@gmail.com>
2026-09-09 22:54:07 +0530

    created the sorting-sub navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Movielist from "./components/Movielist";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -5,7 +6,7 @@ const App = () => {
     <div className="bg-neutral-800 h-screen text-amber-50">
       <div className="conntainer font-supreme font-medium  max-w-300 bg-neutral-800 text-amber-50 mx-auto grid grid-rows-2">
         <Navbar />
-        <main className="main_content h-auto">Main Content</main>
+        <Movielist />
       </div>
     </div>
   );
---
+++ src/components/Movielist.jsx
@@ -0,0 +1,38 @@
+const Movielist = () => {
+  return (
+    <section className="main_content h-auto">
+      <header className="movielist_header flex justify-between h-10 items-center">
+        <h2 className="movielist_heading">Popular 🔥</h2>
+        <div className="movielist_fs flex">
+          <ul className="movie_filter flex gap-2">
+            <li className="movie_filter_item px-2 cursor-pointer active:underline underline">
+              8+ Star
+            </li>
+            <li className="movie_filter_item px-2 cursor-pointer">7+ Star</li>
+            <li className="movie_filter_item px-2 cursor-pointer">6+ Star</li>
+          </ul>
+
+          <select
+            name=""
+            id=""
+            className="movie_sorting mx-2 bg-neutral-500 rounded-md p-1"
+          >
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
+          <select
+            name=""
+            id=""
+            className="movie_sorting bg-neutral-500 rounded-md p-1"
+          >
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Movielist;
